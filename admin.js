4db10d0355feb16324d63e597e9a4a457307f02a
Rune-Lambert <Rune.Lambert@student.tsaam.be>
2024-10-01 12:54:09 +0200

    nog wat commentaar toegevoegd bij admin en index

--- admin.js
+++ admin.js
@@ -1,5 +1,7 @@
+// haal hier informatie uit data
 const {users, rights, userRights} = require("./data");
 
+// functie waar hij wachtwoord controleerd en usernamen en rechten ophaatl
 function login(username, password, cb){
     const user= users.find((user)=> user.username === username);
     
@@ -13,11 +15,11 @@ function login(username, password, cb){
 
     cb(user.username, userRightsDescription.omschrijving)
 }
-
+// functie om het in een string in de terminal te krijgen
 function cb(username, RightsDescription){
     console.log(`${username} : ${RightsDescription}`);
 }
-
+// hier exporteer je de 2 functies
 module.exports = {
    login,
    cb
